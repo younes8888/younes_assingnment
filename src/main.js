41db971d6bb294f9c35c797b7e999b3c17a03a55
Seddouki Younes <seddouki.younes@gmail.com>
2023-11-19 14:15:21 +0100

    younes first commit

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
 // Add your JavaScript here
+
+// Get the display element by ID
+let display = document.getElementById('display');
+
+// Function to append a value to the display
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+// Function to clear the display
+function clearDisplay() {
+    display.value = '';
+}
+
+// Function to calculate and display the result
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
